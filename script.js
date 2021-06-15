d7a0be70e686a08ac0f97b19410e60d6439b1b65
Aaditya Deshmukh <aadityadeshmukh@gmail.com>
2021-06-15 20:02:57 +0530

    working code

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let inpVal = document.getElementById("inpVal");
 inpVal.onchange = () => {
     let imgSrc = document.getElementById("sourceImage");
-    let inputText = inpVal.textContent;
+    let inputText = inpVal.value;
     console.log(inputText);
-    inputText.replace(" ", "%20");
+    inputText = inputText.replace(/ /g, "%20");
     console.log(inputText);
     let baseurl = "https://source.boringavatars.com/beam/240/"
     let trailurl = "?colors=f1ebeb,eee8e8,cacaca,24c0eb,5cceee"
